d249df848909b5bcbb4239d8dcdc83f0a3fd18e4
Michael Lee <leemichael289@gmail.com>
2018-06-26 22:15:20 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,25 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+  return;
+}
+
+function frameIt() {
+  $("img").addClass("tasty")
+  return;
+}
+
+function pressIt(){
+  $('form').on('keydown', function(){
+    if(key.which == 71){
+    alert('You have pressed the "g" key!');
+  }
+  });
+  return;
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
 });
